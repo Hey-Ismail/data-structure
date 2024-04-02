689e22cc26ba29cf4e374f42fb70b26d374e8925
Ismail Hossain <hyethereitsfahim@gmail.com>
2024-04-02 07:48:16 +0600

    finding common elements in two arrays.

---
+++ commonelements.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[] = {1, 4, 6, 3, 6, 9};
+    int B[] = {5, 3, 7, 1, 2, 4};
+
+    bool foundCommon = false;
+
+    for (int i = 0; i < 6; ++i)
+    {
+        for (int j = 0; j < 6; ++j)
+        {
+            if (A[i] == B[j])
+            {
+                cout << A[i] << " ";
+                foundCommon = true;
+                break;
+            }
+        }
+    }
+
+    if (!foundCommon)
+    {
+        cout << "No common element!";
+    }
+
+    cout << endl;
+
+    return 0;
+}
